37e2c8c0ee33443bb252a834ad343a754601eea6
Diego Campanari <campanari@msn.com>
2019-12-06 15:08:26 -0300

    Add Unit Tests

---
+++ NeonHackathon.Tests/PostTests.cs
@@ -0,0 +1,63 @@
+using NUnit.Framework;
+
+namespace NeonHackthon.Tests
+{
+    public class PostTests
+    {
+        [Test]
+        public void ShouldCreateBlog()
+        {
+            
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenCreateBlogWithInvalidUser()
+        {
+
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenCreateBlogWithNoContent()
+        {
+
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenCreateBlogWithNoTitle()
+        {
+
+        }
+
+        [Test]
+        public void ShouldWriteCommentToPost()
+        {
+            
+        }
+
+        [Test]
+        public void ShouldAddTagToPost()
+        {
+            
+        }
+
+        [Test]
+        public void ShouldSearchPostsByTag()
+        {
+
+        }
+
+        [Test]
+        public void ShouldSearchPostsByAuthor()
+        {
+
+        }
+
+        [Test]
+        public void ShouldSearchPostsByTitle()
+        {
+
+        }
+
+
+    }
+}
